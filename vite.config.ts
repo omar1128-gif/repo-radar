78b1b5409b3563babcb5a73003b6cdf97ad61043
Omar Fathy <o_fathy28@yahoo.com>
2026-09-17 16:49:26 +0300

    chore: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+/// <reference types="vitest/config" />
+
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dirname, './src'),
